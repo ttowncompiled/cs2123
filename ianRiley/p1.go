0c56a38ac249306ffd7d4d7106137700a9a6ad94
Ian Riley <ian.riley.915@gmail.com>
2016-01-25 17:18:43 -0600

    Implements Insert and Delete for the binary tree.

--- ianRiley/p1.go
+++ ianRiley/p1.go
@@ -1,6 +1,9 @@
 package main
 
+import "strconv"
+
 type Tree struct {
+  Length int
   Nodes []int
 }
 
@@ -11,14 +14,30 @@ type Binary interface {
 }
 
 func (t *Tree) Insert(value int) {
-  
+  if t.Nodes == nil {
+    t.Nodes = make([]int, 1023, 1023)
+  }
+  t.Nodes[t.Length] = value
+  t.Length++
 }
 
 func (t *Tree) Delete(value int) {
-  
+  if t.Nodes == nil {
+    return
+  }
+  for i := 0; i < t.Length; i++ {
+    if t.Nodes[i] == value {
+      t.Nodes[i] = t.Nodes[t.Length-1]
+      t.Length--
+      return
+    }
+  }
 }
 
 func (t * Tree) ToString() (result string) {
+  if t.Nodes == nil {
+    return
+  }
   return
 }
 
