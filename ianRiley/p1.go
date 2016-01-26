1822c0bdc2630d96f8cc05d893e51f998cf42b5d
Ian Riley <ian.riley.915@gmail.com>
2016-01-26 13:03:50 -0600

    Finishes assignment one.

--- ianRiley/p1.go
+++ ianRiley/p1.go
@@ -7,40 +7,61 @@ type Tree struct {
   Nodes []int
 }
 
-type Binary interface {
-  Delete(value int)
-  Insert(value int)
-  ToString() string
+func Insert(tree *Tree, value int) bool {
+  if tree == nil || tree.Nodes == nil {
+    tree.Nodes = make([]int, 1023, 1023)
+  }
+  tree.Nodes[tree.Length] = value
+  tree.Length++
+  return true
 }
 
-func (t *Tree) Insert(value int) {
-  if t.Nodes == nil {
-    t.Nodes = make([]int, 1023, 1023)
+func Delete(tree *Tree, value int) bool {
+  if tree == nil || tree.Nodes == nil {
+    return false
   }
-  t.Nodes[t.Length] = value
-  t.Length++
-}
-
-func (t *Tree) Delete(value int) {
-  if t.Nodes == nil {
-    return
-  }
-  for i := 0; i < t.Length; i++ {
-    if t.Nodes[i] == value {
-      t.Nodes[i] = t.Nodes[t.Length-1]
-      t.Length--
-      return
+  for i := 0; i < tree.Length; i++ {
+    if tree.Nodes[i] == value {
+      tree.Nodes[i] = tree.Nodes[tree.Length-1]
+      tree.Length--
+      return true
     }
   }
+  return false
 }
 
-func (t * Tree) ToString() (result string) {
-  if t.Nodes == nil {
-    return
+func ToString(tree *Tree, node int) string {
+  if tree == nil || tree.Nodes == nil {
+    return ""
   }
-  return
+  if node >= tree.Length {
+    return ""
+  }
+  left := ToString(tree, 2 * node + 1)
+  if left != "" {
+    left += ", "
+  }
+  right := ToString(tree, 2 * node + 2)
+  if right != "" {
+    right = ", " + right
+  }
+  return left + strconv.Itoa(tree.Nodes[node]) + right
 }
 
 func main() {
-  
+  t := &Tree{}
+  Insert(t, 1)
+  Insert(t, 2)
+  Insert(t, 3)
+  Insert(t, 4)
+  Insert(t, 5)
+  Insert(t, 6)
+  Insert(t, 7)
+  println(ToString(t, 0))
+  Delete(t, 7)
+  println(ToString(t, 0))
+  Delete(t, 3)
+  println(ToString(t, 0))
+  Delete(t, 1)
+  println(ToString(t, 0))
 }
