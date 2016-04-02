125cf1c60825618addb5a83272504172d182c528
Ian Riley <ian.riley.915@gmail.com>
2016-04-01 21:30:42 -0500

    Adds quad and dbl results.

--- hw3/ianRiley/run_hw3.sh
+++ hw3/ianRiley/run_hw3.sh
@@ -7,4 +7,9 @@
 (time go run hw3-quad.go books/tomSawyer.txt quad-results/tomSawyer) > quad-results/tomSawyer.time 2>&1 && \
 (time go run hw3-quad.go books/innocentAdventuress.txt quad-results/innocentAdventuress) > quad-results/innocentAdventuress.time 2>&1 && \
 (time go run hw3-quad.go books/muchAdoAboutNothing.txt quad-results/muchAdoAboutNothing) > quad-results/muchAdoAboutNothing.time 2>&1 && \
-(time go run hw3-quad.go books/prodigalVillage.txt quad-results/prodigalVillage) > quad-results/prodigalVillage.time 2>&1
+(time go run hw3-quad.go books/prodigalVillage.txt quad-results/prodigalVillage) > quad-results/prodigalVillage.time 2>&1 && \
+(time go run hw3-dbl.go books/huckleberryFinn.txt dbl-results/huckleberryFinn) > dbl-results/huckleberryFinn.time 2>&1 && \
+(time go run hw3-dbl.go books/tomSawyer.txt dbl-results/tomSawyer) > dbl-results/tomSawyer.time 2>&1 && \
+(time go run hw3-dbl.go books/innocentAdventuress.txt dbl-results/innocentAdventuress) > dbl-results/innocentAdventuress.time 2>&1 && \
+(time go run hw3-dbl.go books/muchAdoAboutNothing.txt dbl-results/muchAdoAboutNothing) > dbl-results/muchAdoAboutNothing.time 2>&1 && \
+(time go run hw3-dbl.go books/prodigalVillage.txt dbl-results/prodigalVillage) > dbl-results/prodigalVillage.time 2>&1
